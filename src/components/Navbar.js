877ac6c06cf185308b8fbc2bd60929c3d79ab302
Arun solanki <arunsolanki301@gmail.com>
2022-05-22 12:56:26 +0530

    iind

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../screens/styles/NavbarStyles.css";
 
 import { FaSignOutAlt } from "react-icons/fa";
 
 const Navbar = ({ isLoading }) => {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
   const navigate = useNavigate();
   const clearLocal = () => {
     localStorage.clear();
     navigate("/");
   };
+
+  console.log(screenSize);
   return (
     <div className="navbar">
       <img src={require("../assets/logo.png")} className="logo" />
-      <div className="Navlinks">
+      <div className={"Navlinks"}>
         <Link to="/home" className="links">
           Home
         </Link>
@@ -25,7 +28,7 @@ const Navbar = ({ isLoading }) => {
         </Link>
       </div>
       <button className="signout" onClick={clearLocal}>
-        <FaSignOutAlt size="md" />
+        <FaSignOutAlt size={20} />
       </button>
     </div>
   );
